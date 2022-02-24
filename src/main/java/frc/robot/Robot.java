9f9c4c34349160609393548468ebddc3195a6b8a
Jammrock <jammrock@gmail.com>
2022-02-24 18:40:05 -0500

    Added intakebelt code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,20 +9,28 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.XboxController;
+//import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+//import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+//import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
+//import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 
 
 /** This is a demo program showing how to use Mecanum control with the MecanumDrive class. */
 public class Robot extends TimedRobot {
+  // motor channels
   private static final int kFrontLeftChannel = 2;
   private static final int kRearLeftChannel = 3;
   private static final int kFrontRightChannel = 1;
   private static final int kRearRightChannel = 0;
+  private static final int kIntakeBeltChannel = 4;
 
+
+  // controller channels
   private static final int kJoystickChannel = 0;
-
   private static final int kXboxChannel = 1;
 
+  // controls drive limits
   private static final double MAX_Y_SPEED = 1.0;
   private static final double MIN_Y_SPEED = -1.0;
   private static final double MAX_X_SPEED = 1.0;
@@ -30,12 +38,18 @@ public class Robot extends TimedRobot {
   private static final double MAX_Z_ROTATE = 1.0;
   private static final double MIN_Z_ROTATE = -1.0;
 
-  WPI_VictorSPX frontLeft = new WPI_VictorSPX(kFrontLeftChannel);
-  WPI_VictorSPX rearLeft = new WPI_VictorSPX(kRearLeftChannel);
-  WPI_VictorSPX frontRight = new WPI_VictorSPX(kFrontRightChannel);
-  WPI_VictorSPX rearRight = new WPI_VictorSPX(kRearRightChannel);
+  // mechanum motors
+  private WPI_VictorSPX frontLeft = new WPI_VictorSPX(kFrontLeftChannel);
+  private WPI_VictorSPX rearLeft = new WPI_VictorSPX(kRearLeftChannel);
+  private WPI_VictorSPX frontRight = new WPI_VictorSPX(kFrontRightChannel);
+  private WPI_VictorSPX rearRight = new WPI_VictorSPX(kRearRightChannel);
+
+  // launch system motors
+  private final WPI_VictorSPX m_intakeBeltMotor = new WPI_VictorSPX(kIntakeBeltChannel);
+
 
   private MecanumDrive m_robotDrive;
+  //private  m_intakeBelt;
   private Joystick m_stick;
   private XboxController m_Xbox;
 
@@ -56,22 +70,52 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
+    // get drive buttons
     double xJoy = m_stick.getX();
     double yJoy = m_stick.getY();
     double zJoy = m_stick.getZ();
 
-    if (!m_Xbox.getBButton() || (xJoy != 0 && yJoy != 0 && zJoy != 0)){
-      jerryDrivesMech(xJoy, yJoy, zJoy);
-    }
-   else if(m_Xbox.getBButton()){
+    boolean xbcB = m_Xbox.getBButton();
+
+    // drive controls
+    if(xbcB){
      frontLeft.stopMotor();
      frontRight.stopMotor();
      rearLeft.stopMotor();
      rearRight.stopMotor();
-   }
+    }
+    else{
+      jerryDrivesMech(xJoy, yJoy, zJoy);
+    }
+
+    /* original
+    if (!xbcB || (xJoy != 0 && yJoy != 0 && zJoy != 0)){
+      jerryDrivesMech(xJoy, yJoy, zJoy);
+    }
+   else if(xbcB){
+     frontLeft.stopMotor();
+     frontRight.stopMotor();
+     rearLeft.stopMotor();
+     rearRight.stopMotor();
+    }
     else{
       jerryDrivesMech(0,0,0);
     }
+    */
+
+    // intake belt controls
+    /* option 1
+    double intakeSpeed = m_Xbox.getLeftTriggerAxis();
+
+    if (intakeSpeed > 0) {
+      m_intakeBeltMotor.set(intakeSpeed);
+    }
+    else {
+      m_intakeBeltMotor.set(0);
+    */
+
+    //option 2
+    m_intakeBeltMotor.set(m_Xbox.getLeftTriggerAxis());
     
   }
 
